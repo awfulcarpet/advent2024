50435d84a07200b4a98d294d51e1d93c3f0c64df
afwulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2024-11-30 22:52:12 -0700

    part1 finished

--- day1/day1.c
+++ day1/day1.c
@@ -6,27 +6,28 @@ int bigger(const void* a, const void* b) {
 	return *(int *)a - *(int *)b;
 }
 
-#define LEN 6
+#define LEN 1000
 int
 main(void) {
 	FILE *f = NULL;
-	f = fopen("test", "r");
-	char line[15] = {0};
+	f = fopen("input", "r");
+	char line[16] = {0};
 	char num[6] = {0};
-	int left[LEN] = {0};
-	int right[LEN] = {0};
+	int left[LEN + 1] = {0};
+	int right[LEN + 1] = {0};
 	int index = 0;
 	int sum = 0;
-	while (fgets(line, 14, f) != NULL) {
-		int i = 1;
-		for (; line[i] != ' ' && line[i] != '\n'; i++);
+	while (fgets(line, 15, f) != NULL) {
+		int i = 0;
+		printf("%lu\n", strlen(line));
+		for (; line[i] != ' '; i++);
 		memcpy(num, line, i);
 		left[index] = atoi(num);
 		memset(num, 0, 6);
 
-		for (; line[i] == ' ' ; i++);
+		for (; line[i] == ' '; i++);
 		int j = i;
-		for (; line[i] != ' ' && line[i] != '\n'; i++);
+		for (; line[i] <= '9' && line[i] >= '0'; i++);
 		memcpy(num, line + j, i - j);
 		right[index] = atoi(num);
 		memset(num, 0, 6);
@@ -38,7 +39,7 @@ main(void) {
 	qsort(right, LEN, sizeof(int), bigger);
 
 	for(int i = 0; i < index; i++) {
-		/*printf("%d %d\n", left[i], right[i]);*/
+		printf("%d %d\n", left[i], right[i]);
 		sum += abs(left[i] - right[i]);
 	}
 	fclose(f);
